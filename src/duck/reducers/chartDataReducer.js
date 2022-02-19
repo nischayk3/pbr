72c2e8550b33cf9fdae9b481a2a6d3676aae22c8
Dinesh Kumar <RLE0836@rlpl.com>
2022-02-19 15:28:26 +0530

    file upload issue fix

--- src/duck/reducers/chartDataReducer.js
+++ src/duck/reducers/chartDataReducer.js
@@ -6,12 +6,12 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch (action.type) {
-    case types.GET_CHART_DATA:
-        return {
-            ...state,
-            chartName: action.payload,
-        };
-    default:
-        return state;
+        case types.GET_CHART_DATA:
+            return {
+                ...state,
+                chartName: action.payload,
+            };
+        default:
+            return state;
     }
 }
