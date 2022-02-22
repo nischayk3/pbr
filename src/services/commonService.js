b74d9a6568893aa5f73e1655ed51a34afae8f8ce
Dinesh Kumar <RLE0836@rlpl.com>
2022-02-22 13:05:13 +0530

    get view data api integration

---
+++ src/services/commonService.js
@@ -0,0 +1,14 @@
+import Service from './AjaxService';
+import { BMS_APP_PYTHON_SERVICE } from '../constants/apiBaseUrl';
+
+//get view table
+export const getViewTable = (_queryParam) => {
+    return Service.get(BMS_APP_PYTHON_SERVICE + '/views', _queryParam).then(
+        (response) => {
+            return response.data;
+        },
+        (error) => {
+            return error.response.data;
+        }
+    );
+};
