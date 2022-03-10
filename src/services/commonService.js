2ff8a13d0064a19d6a28083b8a8bc8edf245b351
Dinesh Kumar <RLE0836@rlpl.com>
2022-03-10 21:44:29 +0530

    view service changed

--- src/services/commonService.js
+++ src/services/commonService.js
@@ -3,12 +3,12 @@ import { BMS_APP_PYTHON_SERVICE } from '../constants/apiBaseUrl';
 
 //get view table
 export const getViewTable = (_queryParam) => {
-    return Service.get(BMS_APP_PYTHON_SERVICE + '/views', _queryParam).then(
-        (response) => {
-            return response.data;
-        },
-        (error) => {
-            return error.response.data;
-        }
-    );
+  return Service.get(BMS_APP_PYTHON_SERVICE + '/views-list', _queryParam).then(
+    (response) => {
+      return response.data;
+    },
+    (error) => {
+      return error.response.data;
+    }
+  );
 };
