c7e3e4629a57c94de7f6757cfe69800a6227519a
Binkita Tiwari <binkita.tiwari@mareana.com>
2022-03-14 19:02:29 +0530

    fixed the bug

--- src/components/CardComponent/customCard.js
+++ src/components/CardComponent/customCard.js
@@ -9,11 +9,11 @@ const customCard = (props) => {
         <div className={props.count===0?'approval-cards-disabled':props.active===props.desc?'approval-cards-active':'approval-cards'} >
             <div className={props.count===0?'circle_icon-disabled':'circle_icon'} >
                 {props.desc=='View Approval' && <LayoutOutlined />}
-                {props.desc=='Param Approval' && <ProjectOutlined />}
+                {props.desc=='Param Data Approval' && <ProjectOutlined />}
                 {props.desc=='Chart Approval' && <BarChartOutlined />}
                 {props.desc=='Report Approval' && <FileDoneOutlined />}
-                {props.desc=='Pbr Approval' && <FolderOpenOutlined />}
-                {props.desc=='Dtload Approval' && <FileSyncOutlined />}
+                {props.desc=='PBR Approval' && <FolderOpenOutlined />}
+                {props.desc=='Data Load Screen Approval' && <FileSyncOutlined />}
             </div>
             <div className={props.count===0?'card_desc-disabled':'card_desc'}>
                 <p className={props.count===0?'approve-title-disabled':'approve-title'}>{props.count}</p>
