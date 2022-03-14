8f057e004fd54e7a4a631339af5f41a8e6ce1fe4
Dinesh Kumar <RLE0836@rlpl.com>
2022-03-14 17:04:03 +0530

    genealogy fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 import { Button, Input, Layout } from 'antd';
 import { useDispatch } from 'react-redux';
-import {
-    LogoutOutlined,
-  } from '@ant-design/icons';
+import { LogoutOutlined } from '@ant-design/icons';
 import cpvLogo from '../../assets/cpv-logo.png';
 import hamburgerIcon from '../../assets/icons/hamburger.svg';
 import mareanaLogo from '../../assets/mareana_logo.png';
@@ -16,48 +14,36 @@ import { useHistory } from 'react-router-dom';
 const { Header } = Layout;
 const { Search } = Input;
 const HeaderBar = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const toggleCollapsed = () => {
-        dispatch(toggleMenu());
-    };
+  const toggleCollapsed = () => {
+    dispatch(toggleMenu());
+  };
 
-    const Logout = () => {
-        // LOGOUT API NOT WORKING
-        // const jwt = localStorage.getItem('user_token');
-        // await userLogout(jwt);
-        Auth.logout(() => {
-          history.push('/');
-        });
-      };
+  const Logout = () => {
+    // LOGOUT API NOT WORKING
+    // const jwt = localStorage.getItem('user_token');
+    // await userLogout(jwt);
+    Auth.logout(() => {
+      history.push('/');
+    });
+  };
 
-    return (
-        <Header id='header'>
-            <div id='hamburger' className='inline'>
-                <div className='header-logo'>
-                    <img src={mareanaLogo} height='40' alt='menu' />
-                    <span></span>
-                    <img src={cpvLogo} alt='cpv' />
-                </div>
-
-                {/* <Button onClick={toggleCollapsed}>
-					<img src={hamburgerIcon} alt='menu' />
-				</Button> */}
-            </div>
-            <div className='inline' id='search-header'>
-                {/* <Search
-          className="search-secondary"
-          placeholder="Search here..."
-          onSearch={(value) => console.log(value)}
-          enterButton
-        /> */}
-            </div>
-            <div onClick={Logout}>
-            <LogoutOutlined />
-            </div>
-        </Header>
-    );
+  return (
+    <Header id='header'>
+      <div id='hamburger' className='inline'>
+        <div className='header-logo'>
+          <img src={mareanaLogo} height='40' alt='menu' />
+          <span></span>
+          <img src={cpvLogo} alt='cpv' />
+        </div>
+      </div>
+      <div className='logout-btn' onClick={Logout}>
+        <LogoutOutlined />
+      </div>
+    </Header>
+  );
 };
 
 export default HeaderBar;
