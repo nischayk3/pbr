f8ec0966caa77d67fa86b4c8c49b0170ffc5f644
Dinesh Kumar <RLE0836@rlpl.com>
2022-03-20 16:54:38 +0530

    workspace css fixed

--- src/app/dashboard/workspace/components/workspace.js
+++ src/app/dashboard/workspace/components/workspace.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { useDispatch } from 'react-redux';
-import { Card, Table, Input, Space, Avatar, Row, Col } from 'antd';
+import { Card, Table, Input, Space, Avatar, Row, Col, Tabs } from 'antd';
 import illustrations from '../../../../assets/images/Group 33808.svg';
 import DeviationTable from './deviationTable/deviationTable';
 import DataQuality from './dataQuality/dataQuality';
-import { BellOutlined, LayoutOutlined, ArrowRightOutlined } from '@ant-design/icons';
+import {
+  BellOutlined,
+  LayoutOutlined,
+  ArrowRightOutlined,
+} from '@ant-design/icons';
 import {
   hideLoader,
   showLoader,
@@ -14,8 +18,9 @@ import {
 import { getCountData } from '../../../../services/workFlowServices';
 import './styles.scss';
 const { Search } = Input;
-const Workspace = () => {
 
+const { TabPane } = Tabs;
+const Workspace = () => {
   const [resultDate, setResultDate] = useState('');
   const [tilesData, setTilesData] = useState([]);
   const dispatch = useDispatch();
@@ -31,9 +36,9 @@ const Workspace = () => {
     const month = date.toLocaleString('default', { month: 'long' });
     const latestDate = date.getDate();
     const year = date.getFullYear();
-    const resultDate = month + ' ' + latestDate + ',' + ' ' + year
+    const resultDate = month + ' ' + latestDate + ',' + ' ' + year;
     setResultDate(resultDate);
-  }
+  };
 
   const getTilesData = async () => {
     let req = {};
@@ -46,28 +51,35 @@ const Workspace = () => {
       dispatch(hideLoader());
       dispatch(showNotification('error', error.message));
     }
-  }
+  };
   return (
     <div className='custom-wrapper'>
       <div className='workspace-subheader'>
         <Search
-          placeholder="Search"
+          placeholder='Search'
           allowClear
           //onSearch={ }
           style={{ width: 304 }}
         />
         <BellOutlined style={{ margin: '0px 25px' }} />
         <div>
-          <Avatar style={{ backgroundColor: 'orange' }}>{(localStorage.getItem('user')).split("")[0].toUpperCase()} </Avatar>
-          <span style={{ padding: '0px 10px' }}>{(localStorage.getItem('user'))}</span>
+          <Avatar style={{ backgroundColor: 'orange' }}>
+            {localStorage.getItem('user').split('')[0].toUpperCase()}{' '}
+          </Avatar>
+          <span style={{ padding: '0px 10px' }}>
+            {localStorage.getItem('user')}
+          </span>
         </div>
-
       </div>
       <div className='custom-content-layout'>
         <Card className='workspace_head'>
           <div>
-            <p className='workspace-username'>Howdy {(localStorage.getItem('user'))}! Good Morning</p>
-            <p className='workspace-text'>Let's see what you have on your plate today!</p>
+            <p className='workspace-username'>
+              Howdy {localStorage.getItem('user')}! Good Morning
+            </p>
+            <p className='workspace-text'>
+              Let's see what you have on your plate today!
+            </p>
           </div>
           <div style={{ display: 'flex', flexDirection: 'row', flex: 1 }}>
             <img src={illustrations} className='workspace-illustration' />
@@ -75,103 +87,168 @@ const Workspace = () => {
           <div>
             <span className='workspace-resultdate'>{resultDate}</span>
           </div>
-
         </Card>
         <div className='workspace-wrapper'>
           <div className='workspace-main-block'>
             <div className='workspace-innerColumn'>
               <div className='workspace-card1'>
                 <div className='innercard'>
-                  <LayoutOutlined style={{ color: '#0CE7CC', fontSize: '15px' }} />
+                  <LayoutOutlined
+                    style={{ color: '#0CE7CC', fontSize: '15px' }}
+                  />
                   <span className='deviation-text'> Workflow Approvals</span>
                   <span style={{ float: 'right' }}>
-                    <ArrowRightOutlined style={{ color: '#0CE7CC', fontSize: '15px' }} />
-                    <a className='workspace-review' onClick={() => history.push('/dashboard/workflow')}>Review</a>
+                    <ArrowRightOutlined
+                      style={{ color: '#0CE7CC', fontSize: '15px' }}
+                    />
+                    <a
+                      className='workspace-review'
+                      onClick={() => history.push('/dashboard/workflow')}
+                    >
+                      Review
+                    </a>
                   </span>
 
                   <Row gutter={4}>
                     {tilesData.map((item, index) => {
                       return (
-                        <Col className="gutter-row" span={4}>
+                        <Col className='gutter-row' span={4}>
                           {item.item_count > 0 && (
                             <div style={{ marginTop: '15px' }} key={index}>
-                              <p className='approval-text'>{item.text.split(" ")[0]}</p>
-                              <p className='approval-count'>{item.item_count}</p>
+                              <p className='approval-text'>
+                                {item.text.split(' ')[0]}
+                              </p>
+                              <p className='approval-count'>
+                                {item.item_count}
+                              </p>
                             </div>
                           )}
                         </Col>
-                      )
-
+                      );
                     })}
                   </Row>
                 </div>
-                <div>
+                <div className='avatar-block'>
                   <p>5 People awaiting your approval!</p>
                   <Avatar.Group
-                    maxCount={2}
+                    maxCount={4}
                     maxStyle={{
-                      color: '#f56a00',
+                      color: '#0CE7CC',
                       backgroundColor: '#fde3cf',
                     }}
                   >
                     <Avatar
                       style={{
-                        backgroundColor: '#f56a00',
+                        backgroundColor: '#0CE7CC',
                       }}
                     >
-                      V
+                      A
+                    </Avatar>
+                    <Avatar
+                      style={{
+                        backgroundColor: '#0CE7CC',
+                      }}
+                    >
+                      B
+                    </Avatar>
+                    <Avatar
+                      style={{
+                        backgroundColor: '#0CE7CC',
+                      }}
+                    >
+                      C
+                    </Avatar>
+                    <Avatar
+                      style={{
+                        backgroundColor: '#0CE7CC',
+                      }}
+                    >
+                      D
+                    </Avatar>
+                    <Avatar
+                      style={{
+                        backgroundColor: '#0CE7CC',
+                      }}
+                    >
+                      E
                     </Avatar>
                   </Avatar.Group>
                 </div>
               </div>
               <div className='workspace-card2'>
                 <div className='innercard'>
-                  <LayoutOutlined style={{ color: '#0CE7CC', fontSize: '15px' }} />
+                  <LayoutOutlined
+                    style={{ color: '#0CE7CC', fontSize: '15px' }}
+                  />
                   <span className='deviation-text'>Paper Batch Records</span>
                   <span style={{ float: 'right' }}>
-                    <ArrowRightOutlined style={{ color: '#0CE7CC', fontSize: '15px' }} />
-                    <a onClick={() => console.log('PBR')} className='workspace-review'>View All</a>
+                    <ArrowRightOutlined
+                      style={{ color: '#0CE7CC', fontSize: '15px' }}
+                    />
+                    <a
+                      onClick={() => console.log('PBR')}
+                      className='workspace-review'
+                    >
+                      View All
+                    </a>
                   </span>
                   <div className='paper-batch-card'>
                     <p className='paper-batch-count'>5</p>
-                    <p className='paper-batch-desc'>New paper batch records are awaiting your approval!</p>
+                    <p className='paper-batch-desc'>
+                      New paper batch records are awaiting your approval!
+                    </p>
                   </div>
                 </div>
-
               </div>
             </div>
             <div className='workspace-chart'>
               <div className='innercard'>
-                <LayoutOutlined style={{ color: '#0CE7CC', fontSize: '15px' }} />
+                <LayoutOutlined
+                  style={{ color: '#0CE7CC', fontSize: '15px' }}
+                />
                 <span className='deviation-text'>Process Control Charts</span>
+                <div>
+                  <Tabs
+                    className='workspace-tabs'
+                    // activeKey={activeTab}
+                    // onChange={changeTab}
+                  >
+                    <TabPane tab='Awaiting Approval' key='1'>
+                      <p>content 1</p>
+                    </TabPane>
+                    <TabPane tab='Recently Approved' key='2'>
+                      <p>content 2</p>
+                    </TabPane>
+                  </Tabs>
+                </div>
               </div>
             </div>
-
           </div>
 
           <div className='workspace-outerColumn'>
             <div className='workspace-table1'>
               <div className='innercard'>
-                <LayoutOutlined style={{ color: '#0CE7CC', fontSize: '15px' }} />
+                <LayoutOutlined
+                  style={{ color: '#0CE7CC', fontSize: '15px' }}
+                />
                 <span className='deviation-text'> Recent Deviations</span>
                 <DeviationTable />
               </div>
-
             </div>
             <div className='workspace-table2'>
               <div className='innercard'>
-                <LayoutOutlined style={{ color: '#0CE7CC', fontSize: '15px' }} />
+                <LayoutOutlined
+                  style={{ color: '#0CE7CC', fontSize: '15px' }}
+                />
                 <span className='deviation-text'> Data Quality</span>
                 <DataQuality />
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default Workspace;
