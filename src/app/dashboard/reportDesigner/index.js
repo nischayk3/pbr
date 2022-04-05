3910132e3ef0f6fc18b1fafca14629e4b3d33435
Mihir Bagga <mihir.bagga@mareana.com>
2022-04-05 17:47:27 +0530

    report designer aand generator UI

--- src/app/dashboard/reportDesigner/index.js
+++ src/app/dashboard/reportDesigner/index.js
@@ -1,26 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import DesignCharts from './components/reportDesignerNew';
 import ReportGenerator from '../reportGenerator/components/reportGenerator';
 import {useSelector,useDispatch} from 'react-redux'
 import Landing from './components/reportLanding'
 import ChartTable from '../../../components/ChartTable';
+import ReportHead from './component/reportLanding/reportHead';
 
 function Report()
 {
     const screen = useSelector((state)=>state.reportDesignerReducer.screen)
+    const [screenChange, setScreenChange] = useState(false)
 
     useEffect(()=>
     {
 
     },[])
+
+    const changeScreen = () => {
+        setScreenChange(true)
+    }
     return (
         <div>
-            {/* <Landing/> */}
-            {screen ?
+           {!screenChange ? <Landing changeScreen={changeScreen} /> : 
+           <> {screen ?
            <div><ReportGenerator/></div> :
           <div><DesignCharts/></div> 
 
-            }
+            } </> }
             
 
         </div>
