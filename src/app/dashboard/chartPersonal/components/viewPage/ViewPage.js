98555f529712fc8204300d69639342c29a3ac658
Binkita Tiwari <binkita.tiwari@mareana.com>
2022-04-20 15:02:17 +0530

    integrated publish in chart personalization

--- src/app/dashboard/chartPersonal/components/viewPage/ViewPage.js
+++ src/app/dashboard/chartPersonal/components/viewPage/ViewPage.js
@@ -35,6 +35,7 @@ import AlertEvaluation from "../scheduled-alerts/alertEvaluation";
 //schedule-alert table
 import AlertTable from "../scheduled-alerts/scheduledAlertsTable";
 import JobSchedule from "../../../../../components/JobSchedule";
+import Signature from '../../../../../components/ElectronicSignature/signature';
 
 const { TabPane } = Tabs;
 
@@ -46,6 +47,9 @@ const ViewPage = () => {
   //state for chart json data
   const [postChartData, setPostChartData] = useState({});
   const [alertModal, setAlertModal] = useState(false);
+  const [isPublish, setIsPublish] = useState(false);
+  const [publishResponse, setPublishResponse] = useState({});
+  const [approveReject, setApproveReject] = useState('');
 
   const dispatch = useDispatch();
 
@@ -123,6 +127,16 @@ const ViewPage = () => {
     }
   };
 
+  const handleClose = () => {
+    setIsPublish(false);
+  };
+
+  const PublishResponse = res => {
+    console.log(res);
+    setPublishResponse(res);
+
+  };
+
   //function for getting chart data
   const getChart = async () => {
     const req = { chartId: id };
@@ -169,10 +183,29 @@ const ViewPage = () => {
         <div className="btns">
           <Button onClick={() => setAlertModal(true)}>Schedule Alert</Button>
           <Button onClick={() => saveAs("save")}>Save</Button>
-          <Button>
+          <Button onClick={() => {
+            setIsPublish(true);
+            setApproveReject('P');
+          }}>
             {" "}
             <CloudUploadOutlined />
             Publish
+          </Button>
+          <Button
+            onClick={() => {
+              setIsPublish(true);
+              setApproveReject('R');
+            }}
+          >
+            Reject
+          </Button>
+          <Button
+            onClick={() => {
+              setIsPublish(true);
+              setApproveReject('A');
+            }}
+          >
+            Approve
           </Button>
           <Dropdown overlay={menu}>
             <MoreOutlined />
@@ -270,6 +303,16 @@ const ViewPage = () => {
         </Tabs>
       </Modal>
       <JobSchedule visible={alertModal} app_type='Chart' handleCancel={handleCancel} id={'reportId'} />
+      <Signature
+        isPublish={isPublish}
+        handleClose={handleClose}
+        screenName='Chart Personalization'
+        PublishResponse={PublishResponse}
+        appType='CHART'
+        dispId={postChartData.data && postChartData.data[0].chart_id}
+        version={postChartData.data && postChartData.data[0].chart_version}
+        status={approveReject}
+      />
     </div>
   );
 };
