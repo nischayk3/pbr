e5299f857c0f6e0b10ea24a93b7ce109420f9d45
Mihir Bagga <mihir.bagga@mareana.com>
2022-05-04 11:19:11 +0530

    Job scheduling bug fix

--- src/components/JobSchedule/scheduled-alerts/alertEvaluation.js
+++ src/components/JobSchedule/scheduled-alerts/alertEvaluation.js
@@ -59,6 +59,16 @@ const alertEvaluation = (props) => {
     const [selectedEmailTimeRange, setSelectedEmailTimeRange] = useState('');
     const [emailLoad, setEmailLoad] = useState({})
 
+    let days_obj = {
+        'Sunday': 0,
+        'Monday': 1,
+        'Tuesday': 2,
+        'Wednesday': 3,
+        'Thursday': 4,
+        'Friday': 5,
+        'Saturday': 6
+    }
+
 
 
     useEffect(() => {
