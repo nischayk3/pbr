0469712bad88b26391802f4c7044bfb42a609ed0
Mihir Bagga <mihir.bagga@mareana.com>
2022-05-11 18:21:07 +0530

    view screen fix

--- src/app/dashboard/viewScreen/components/materialTree/index.js
+++ src/app/dashboard/viewScreen/components/materialTree/index.js
@@ -40,6 +40,7 @@ const MaterialTree = props => {
 		setCheckedKeys(checkedKeys);
 	};
 	const handleClickParam = (e, keys, param, record) => {
+		console.log( keys, param)
 		let rowData = {};
 		let batchData = {};
 		let newBatchData = {};
@@ -97,7 +98,7 @@ const MaterialTree = props => {
 	return (
 		<>
 			{treeMap &&
-				treeMap.map(item => {
+				treeMap.map((item,index) => {
 					return (
 						<Tree
 							//	checkable
@@ -106,11 +107,11 @@ const MaterialTree = props => {
 							checkedKeys={checkedKeys}
 							selectedKeys={selectedKeys}
 							className='custom-treenode'>
-							<TreeNode title={item.process_step} key={item.key}>
-								{item.children.map(a => {
+							<TreeNode title={item.process_step} key={item.key+index}>
+								{item.children.map((a,index) => {
 									return (
-										<TreeNode title={a.product_description} key={a.key}>
-											{a.children.map(b => {
+										<TreeNode title={a.product_description} key={a.key+index}>
+											{a.children.map((b,index) => {
 												return (
 													<TreeNode
 														title={
@@ -140,7 +141,7 @@ const MaterialTree = props => {
 																</span>
 															</div>
 														}
-														key={b.key}
+														key={b.key+index}
 													/>
 												);
 											})}
