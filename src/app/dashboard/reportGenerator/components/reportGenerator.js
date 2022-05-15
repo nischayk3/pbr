347e74288f6c64fd2e891f2dabcd72e65117e613
Mihir Bagga <mihir.bagga@mareana.com>
2022-05-15 19:44:37 +0530

    report and view fix

--- src/app/dashboard/reportGenerator/components/reportGenerator.js
+++ src/app/dashboard/reportGenerator/components/reportGenerator.js
@@ -173,6 +173,8 @@ function ReportGenerator(props) {
 
     const makeArrayOfObject = (ar) => {
         let res = []
+        if(ar && ar.length > 0)
+        {
         for (let i = 0; i < ar.length; i++) {
             let res_obj = {}
             res_obj['chart'] = ar[i]
@@ -185,6 +187,9 @@ function ReportGenerator(props) {
             res.push(res_obj)
         }
         return res
+    }
+    else
+    return res
     }
     const createChartRecord = (arr) => {
         let res = {}
