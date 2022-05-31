105b7c78298a83fdae2b252390e4f37d12ed3462
Vinay <vinay.palagati1.com>
2022-05-31 19:10:47 +0530

    fixed-sonar-cube-issues

--- src/app/dashboard/chartPersonal/components/ChartPersonal.js
+++ src/app/dashboard/chartPersonal/components/ChartPersonal.js
@@ -1,35 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.scss";
 //antd imports
-import { Modal, Tabs } from "antd";
-import {
-  ArrowLeftOutlined,
-  DesktopOutlined,
-  ArrowRightOutlined,
-} from "@ant-design/icons";
+import { Tabs } from "antd";
 //components
 import LandingPage from "./landingPage/LandingPage";
-//react-redux
-import { useDispatch } from "react-redux";
 import BreadCrumbWrapper from "../../../../components/BreadCrumbWrapper";
-//schedule-alert table
-import AlertTable from "./scheduled-alerts/scheduledAlertsTable";
-//alert evaluation
-import AlertEvaluation from "./scheduled-alerts/alertEvaluation";
 
 const { TabPane } = Tabs;
 
 //main component
 const ChartPersonal = () => {
-  //for showing schedule alert modal
-  const [alertModal, setAlertModal] = useState(false);
-
-  const dispatch = useDispatch();
-
-  const handleCancel = () => {
-    setAlertModal(false);
-  };
-
   return (
     <div className="custom-wrapper bread-wrap">
       <div className="sub-header">
