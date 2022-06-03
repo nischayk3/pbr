d121d38ff1306f5a7223728e13bb6c6c63de67c8
Binkita Tiwari <binkita.tiwari@mareana.com>
2022-06-03 12:55:07 +0530

    dashboard test cases

--- cypress/integration/Dashboard.js
+++ cypress/integration/Dashboard.js
@@ -3,7 +3,9 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 });
 
 describe('chart Personalization', () => {
-	it("should login successfully using Ad", () => {
+	// it("should login successfully using Ad", () => {
+	beforeEach(() => {
+		cy.viewport(1280, 720);
 		cy.visit("/");
 		cy.url().should("include", "/user/login");
 		localStorage.setItem("test_enabled", true);
@@ -22,8 +24,8 @@ describe('chart Personalization', () => {
 					"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IkZhaGFkIFNpZGRpcXVpIiwidW5peF90aW1lc3RhbXAiOjE2NDg0NTQ4OTUuMzc5OTQzLCJ0aW1lc3RhbXAiOiIyOC8wMy8yMDIyIDA4OjA4OjE1IiwiZXhwIjo0ODAyMDU0ODk1LCJhZF9yb2xlIjpmYWxzZSwibWRoX3JvbGUiOiJVU0VSIiwiZW1haWxfaWQiOiJmYWhhZC5zaWRkaXF1aUBtYXJlYW5hLmNvbSIsImN1c3Rfa2V5IjoiMTAwMCJ9.pP2tG-5PmpqozTuX1-q_GwEkvYkigrxLWGyUcgP-CDc"
 			})
 		);
-
-
+	})
+	it("should login successfully using Ad", () => {
 		cy.get("#login-btn", { timeout: 2000 }).click();
 		cy.get('#dashboard > .ant-menu-title-content > a', { timeout: 20000 }).click({ force: true });
 		cy.location('href', { timeout: 10000 }).should('include', '/dashboard');
@@ -45,21 +47,14 @@ describe('chart Personalization', () => {
 		cy.get(':nth-child(3) > .ant-breadcrumb-link > a').click();
 		cy.get(':nth-child(1) > .chart-tiles > .chartName').click();
 		cy.get('[style="margin-left: 20px; margin-right: 20px;"] > .anticon > svg').click();
-		cy.get('.ant-input').clear();
-		cy.get('.ant-input').type('C248{enter}');
-		cy.get('.ant-table-row > :nth-child(1)').click();
-		cy.get('.ant-col > .ant-btn > span').click();
-		cy.get('[style="margin-left: 20px; margin-right: 20px;"] > .anticon > svg').click();
-		cy.get('[d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z"]').click();
-		cy.get('.ant-input').clear();
-		cy.get('.ant-input').type('249{enter}');
-		cy.get('.ant-table-row > :nth-child(2)').click();
 		cy.get('.select_field > .ant-select > .ant-select-selector > .ant-select-selection-item').click();
 		cy.get('.ant-select-item-option-active > .ant-select-item-option-content').click();
 		cy.get('.ant-col > .ant-btn > span').click();
-		cy.get('.inner-chart-filters > div > [style="margin-left: 20px; margin-right: 20px;"] > .anticon > svg').click();
+		cy.get('[style="margin-left: 20px; margin-right: 20px;"] > .anticon > svg').click();
 		cy.get('.btns > :nth-child(2) > span').click();
 		/* ==== End Cypress Studio ==== */
+
+
 
 	});
 });
