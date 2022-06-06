25dcb4d2e72630910db01525fea3a8c754b2fff1
Vinay <vinay.palagati1.com>
2022-06-06 14:35:44 +0530

    fixed-duplicacy-part

--- src/app/dashboard/chartPersonal/components/scheduled-alerts/scheduledAlertsTable.js
+++ src/app/dashboard/chartPersonal/components/scheduled-alerts/scheduledAlertsTable.js
@@ -1,50 +1,52 @@
-import React from 'react';
-import {Table} from 'antd';
+import React from "react";
+import { Table } from "antd";
 
 export default function scheduledAlertsTable() {
-    const columns = [
-        {
-            title: 'Job ID',
-            key: 'view_name',
-            dataIndex: 'view_name',
-        },
-        {
-            title: 'Job Name',
-            key: 'view_disp_id',
-            dataIndex: 'view_disp_id',
-        },
-        {
-            title: 'Job Description',
-            key: 'product_num',
-            dataIndex: 'product_num',
-        },
-        {
-            title: 'Job Description',
-            key: 'product_num',
-            dataIndex: 'product_num',
-        },
-        {
-            title: 'Schedule',
-            key: 'product_num',
-            dataIndex: 'product_num',
-        },
-        {
-            title: 'Status',
-            key: 'product_num',
-            dataIndex: 'product_num',
-        },
-        {
-            title: 'Creator',
-            key: 'product_num',
-            dataIndex: 'product_num',
-        }
-    ]
-    return (
-        <Table
-        bordered={false}
-        columns={columns}
-        dataSource={[]}
-        pagination={false}
-        scroll={{ y: 150 }}/>
-    )
+  const columns = [
+    {
+      title: "Job ID",
+      key: "view_name",
+      id: "view_name",
+      dataIndex: "view_name",
+    },
+    {
+      title: "Job Name",
+      key: "view_disp_id",
+      id: "view_disp_id",
+      dataIndex: "view_disp_id",
+    },
+    {
+      title: "Job Description",
+      key: "product_num",
+      id: "product_num",
+      dataIndex: "product_num",
+    },
+    {
+      title: "Schedule",
+      key: "product_num",
+      id: "product_num",
+      dataIndex: "product_num",
+    },
+    {
+      title: "Status",
+      key: "product_num",
+      id: "product_num",
+      dataIndex: "product_num",
+    },
+    {
+      title: "Creator",
+      key: "product_num",
+      id: "product_num",
+      dataIndex: "product_num",
+    },
+  ];
+  return (
+    <Table
+      bordered={false}
+      columns={columns}
+      dataSource={[]}
+      pagination={false}
+      scroll={{ y: 150 }}
+    />
+  );
 }
