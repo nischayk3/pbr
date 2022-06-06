7cbe7f33b759a7c0d185333e1c68b1922d7462b8
Binkita Tiwari <binkita.tiwari@mareana.com>
2022-06-06 17:18:39 +0530

    test cases for dashboard and workflow

--- cypress/integration/Dashboard.js
+++ cypress/integration/Dashboard.js
@@ -2,7 +2,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 	return false;
 });
 
-describe('chart Personalization', () => {
+describe('Dashboard', () => {
 	// it("should login successfully using Ad", () => {
 	beforeEach(() => {
 		cy.viewport(1280, 720);
@@ -10,6 +10,7 @@ describe('chart Personalization', () => {
 		cy.url().should("include", "/user/login");
 		localStorage.setItem("test_enabled", true);
 		localStorage.setItem("user", "fahad.siddiqui@mareana.com");
+		localStorage.setItem("username", "Fahad");
 		localStorage.setItem(
 			"login_details",
 			JSON.stringify({
@@ -25,11 +26,50 @@ describe('chart Personalization', () => {
 			})
 		);
 	})
+	it('Load View Landing Page Correctly', () => {
+		cy.intercept('GET', '**/dashboards', { fixture: 'dashboard.json' })
+		cy.visit('http://localhost:3030/#/dashboard/dashboard')
+
+		cy.log('Load Landing Page')
+		cy.url().should('eq', 'http://localhost:3030/#/dashboard/dashboard')
+	})
+
+	it('Load Screen Header', () => {
+		cy.get("#login-btn", { timeout: 2000 }).click();
+		cy.get('#dashboard > .ant-menu-title-content > a', { timeout: 20000 }).click({ force: true });
+		cy.location('href', { timeout: 10000 }).should('include', '/dashboard');
+		const date = new Date();
+		const month = date.toLocaleString('default', { month: 'long' });
+		const latestDate = date.getDate();
+		const year = date.getFullYear();
+		const currentDate = month + ' ' + latestDate + ',' + ' ' + year;
+
+		cy.log('Verify Screen Header Component')
+		cy.get('.screen_header_head')
+
+		cy.log('Verify User Name')
+		cy.get('.screen_header_username').should("have.text", "Howdy Fahad,")
+
+		cy.log('Verify Header Text')
+		cy.get('.screen_header_text').should("have.text", "Lets get designing some report templates")
+
+		cy.log('Verify Current Date')
+		cy.get('.screen_header_resultdate').should("have.text", currentDate)
+	})
+
+	it('Recently Created Dashboard', () => {
+		cy.log('Recent View Verify')
+		cy.get("#login-btn", { timeout: 2000 }).click();
+		cy.get('#dashboard > .ant-menu-title-content > a', { timeout: 20000 }).click({ force: true });
+		cy.location('href', { timeout: 10000 }).should('include', '/dashboard');
+		cy.wait(6000);
+		cy.get(':nth-child(1) > .chart-tiles').should('have.length', 1)
+	})
 	it("should login successfully using Ad", () => {
 		cy.get("#login-btn", { timeout: 2000 }).click();
 		cy.get('#dashboard > .ant-menu-title-content > a', { timeout: 20000 }).click({ force: true });
 		cy.location('href', { timeout: 10000 }).should('include', '/dashboard');
-		cy.get('.create-new > .anticon > svg').click();
+		cy.get('.create-new > .anticon > svg', { timeout: 10000 }).click({ force: true });
 		cy.get(':nth-child(1) > :nth-child(2) > :nth-child(1) > .ant-input').clear();
 		cy.get(':nth-child(1) > :nth-child(2) > :nth-child(1) > .ant-input').type('new dashboard');
 		cy.get('.ant-input-wrapper > .ant-input').click();
@@ -45,7 +85,8 @@ describe('chart Personalization', () => {
 		cy.get('[style="background-color: rgb(9, 49, 133); color: white; border-radius: 4px;"] > span').click();
 		/* ==== Generated with Cypress Studio ==== */
 		cy.get(':nth-child(3) > .ant-breadcrumb-link > a').click();
-		cy.get(':nth-child(1) > .chart-tiles > .chartName').click();
+		cy.wait(6000);
+		cy.get(':nth-child(1) > .chart-tiles').click();
 		cy.get('[style="margin-left: 20px; margin-right: 20px;"] > .anticon > svg').click();
 		cy.get('.select_field > .ant-select > .ant-select-selector > .ant-select-selection-item').click();
 		cy.get('.ant-select-item-option-active > .ant-select-item-option-content').click();
@@ -57,4 +98,6 @@ describe('chart Personalization', () => {
 
 
 	});
+
+
 });
