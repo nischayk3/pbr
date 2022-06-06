3de3550d54d20a04508fb16279d2f23921effd08
Dinesh Kumar <RLE0836@rlpl.com>
2022-06-06 17:06:57 +0530

    view creation testcases

--- cypress/integration/ViewCreation.js
+++ cypress/integration/ViewCreation.js
@@ -32,10 +32,10 @@ describe("Render View Creation Page", () => {
 
 	it('Load View Landing Page Correctly', () => {
 		cy.intercept('GET', '**/views-list', { fixture: 'viewList.json' })
-		cy.visit('http://localhost:3030/#/dashboard/view_creation')
+		cy.visit('http://localhost/#/dashboard/view_creation')
 
 		cy.log('Load Landing Page')
-		cy.url().should('eq', 'http://localhost:3030/#/dashboard/view_creation')
+		cy.url().should('eq', 'http://localhost/#/dashboard/view_creation')
 	})
 
 
@@ -83,52 +83,56 @@ describe("Render View Creation Page", () => {
 		cy.wait(5000)
 	})
 
-	it('Load View Creation Page Correctly', () => {
-
+	it('Render Parameter Lookup', () => {
 		cy.log('Select a Molecule')
 		cy.get('#rc_select_0').click();
 		cy.get('div[title="BELATACEPT"]').click({ force: true })
 		cy.wait(5000)
-
-		cy.log('Select a Parameter From View Hierarchy')
-		cy.get(':nth-child(1) > .ant-tree-list > .ant-tree-list-holder > :nth-child(1) > .ant-tree-list-holder-inner > .ant-tree-treenode > .ant-tree-switcher > .anticon > svg').click({ force: true });
-		cy.wait(4000)
-		cy.get(':nth-child(2) > .ant-tree-switcher > .anticon > svg').click({ force: true });
-		cy.wait(4000)
-		cy.get(':nth-child(4) > .ant-tree-node-content-wrapper > .ant-tree-title > .treenode-block > :nth-child(2) > .anticon > svg').click();
-		cy.wait(4000)
-		cy.get(':nth-child(5) > .ant-tree-node-content-wrapper > .ant-tree-title > .treenode-block > :nth-child(2)').click();
-		cy.wait(4000)
-
-		cy.get('.add-var_block > div').click();
-		cy.wait(4000)
-
-		cy.get('[data-row-key="140L-1176024-ASSAY 1DECPT"] > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').check();
-		cy.get('[data-row-key="140L-1176024-ASSAY 1DECPT"] > [style="position: sticky; left: 182px;"] > .ant-radio-wrapper > .ant-radio > .ant-radio-inner').check();
-		cy.wait(2000)
-
-		cy.get('#rc_select_3').click();
-		cy.get(':nth-child(5) > :nth-child(1) > .ant-select-dropdown > :nth-child(1) > .rc-virtual-list > .rc-virtual-list-holder > :nth-child(1) > .rc-virtual-list-holder-inner > .ant-select-item-option-active > .ant-select-item-option-content').click();
-		cy.wait(4000)
-
-		cy.get('.add-var_block > .ant-btn > span').click();
-		cy.get('.input_field > .ant-input').clear();
-		cy.get('.input_field > .ant-input').type('var1');
-		cy.get('.variable-name-popup > .ant-btn > span').click();
-		cy.get('[data-row-key="140L-1176024-ASSAY 1DECPT"] > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').check();
-		cy.get('.add-var_block > .ant-btn > span').click();
-		cy.get('.input_field > .ant-input').clear();
-		cy.get('.input_field > .ant-input').type('var2');
-		cy.get('.variable-name-popup > .ant-btn > span').click();
-		cy.get('.w-tc-editor-text').click();
-		cy.get(':nth-child(2) > .custom-eval-btn > span').click();
-		cy.get('.ant-modal-close-x > .anticon > svg').click();
-		cy.get('.custom-secondary-eval-btn > span').click();
-		cy.get('.function-input > .input_field > .ant-input').clear();
-		cy.get('.function-input > .input_field > .ant-input').type('func1');
-		cy.get('.custom-secondary-btn > span').click();
-		cy.get('.summary-column > p').click();
-		cy.get('.summary-column > :nth-child(2) > .anticon > svg').click();
 	})
 
+	// 	it('Load View Creation Page Correctly', () => {
+
+
+
+
+	// 		cy.log('Select a Parameter From View Hierarchy')
+	// 		cy.get(':nth-child(1) > .ant-tree-list > .ant-tree-list-holder > :nth-child(1) > .ant-tree-list-holder-inner > .ant-tree-treenode > .ant-tree-switcher > .anticon > svg').click({ force: true });
+	// 		cy.wait(4000)
+	// 		cy.get(':nth-child(2) > .ant-tree-switcher > .anticon > svg').click({ force: true });
+	// 		cy.wait(4000)
+	// 		cy.get(':nth-child(4) > .ant-tree-node-content-wrapper > .ant-tree-title > .treenode-block > :nth-child(2) > .anticon > svg').click();
+	// 		cy.wait(4000)
+	// 		cy.get(':nth-child(5) > .ant-tree-node-content-wrapper > .ant-tree-title > .treenode-block > :nth-child(2)').click();
+	// 		cy.wait(4000)
+
+	// 		cy.get('.add-var_block > div').click();
+	// 		cy.wait(4000)
+
+	// 		cy.get('[data-row-key="140L-1176024-ASSAY 1DECPT"] > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').check();
+	// 		cy.get('[data-row-key="140L-1176024-ASSAY 1DECPT"] > [style="position: sticky; left: 182px;"] > .ant-radio-wrapper > .ant-radio > .ant-radio-inner').check();
+	// 		cy.wait(2000)
+
+	// 		cy.get('#rc_select_3').click();
+	// 		cy.get(':nth-child(5) > :nth-child(1) > .ant-select-dropdown > :nth-child(1) > .rc-virtual-list > .rc-virtual-list-holder > :nth-child(1) > .rc-virtual-list-holder-inner > .ant-select-item-option-active > .ant-select-item-option-content').click();
+	// 		cy.wait(4000)
+
+	// 		cy.get('.add-var_block > .ant-btn > span').click();
+	// 		cy.get('.input_field > .ant-input').clear();
+	// 		cy.get('.input_field > .ant-input').type('var1');
+	// 		cy.get('.variable-name-popup > .ant-btn > span').click();
+	// 		cy.get('[data-row-key="140L-1176024-ASSAY 1DECPT"] > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').check();
+	// 		cy.get('.add-var_block > .ant-btn > span').click();
+	// 		cy.get('.input_field > .ant-input').clear();
+	// 		cy.get('.input_field > .ant-input').type('var2');
+	// 		cy.get('.variable-name-popup > .ant-btn > span').click();
+	// 		cy.get('.w-tc-editor-text').click();
+	// 		cy.get(':nth-child(2) > .custom-eval-btn > span').click();
+	// 		cy.get('.ant-modal-close-x > .anticon > svg').click();
+	// 		cy.get('.custom-secondary-eval-btn > span').click();
+	// 		cy.get('.function-input > .input_field > .ant-input').clear();
+	// 		cy.get('.function-input > .input_field > .ant-input').type('func1');
+	// 		cy.get('.custom-secondary-btn > span').click();
+	// 		cy.get('.summary-column > p').click();
+	// 		cy.get('.summary-column > :nth-child(2) > .anticon > svg').click();
+	// 	})
 });
