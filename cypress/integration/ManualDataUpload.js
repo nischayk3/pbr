f6416223b0cb30fb6d00941fb8f3cb88ca0ba638
Md Fahad Iqbal. Siddiqui <RLE0392@rlpl.com>
2022-06-10 11:39:53 +0530

    manual data upload

--- cypress/integration/ManualDataUpload.js
+++ cypress/integration/ManualDataUpload.js
@@ -4,8 +4,6 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('Manual data upload', () => {
 	it("should login successfully using Ad", () => {
-        cy.visit("/");
-        cy.url().should("include", "/user/login");
         localStorage.setItem("test_enabled", true);
         localStorage.setItem("user", "fahad.siddiqui@mareana.com");
         localStorage.setItem(
@@ -15,32 +13,42 @@ describe('Manual data upload', () => {
             email_id: "fahad.siddiqui@mareana.com",
             firstname: "Fahad",
             lastname: "siddiqui",
-            email_id: "fahad.siddiqui@mareana.com",
             mdh_role: "USER",
             screen_set: "1000_USER",
             token:
               "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IkZhaGFkIFNpZGRpcXVpIiwidW5peF90aW1lc3RhbXAiOjE2NDg0NTQ4OTUuMzc5OTQzLCJ0aW1lc3RhbXAiOiIyOC8wMy8yMDIyIDA4OjA4OjE1IiwiZXhwIjo0ODAyMDU0ODk1LCJhZF9yb2xlIjpmYWxzZSwibWRoX3JvbGUiOiJVU0VSIiwiZW1haWxfaWQiOiJmYWhhZC5zaWRkaXF1aUBtYXJlYW5hLmNvbSIsImN1c3Rfa2V5IjoiMTAwMCJ9.pP2tG-5PmpqozTuX1-q_GwEkvYkigrxLWGyUcgP-CDc"
           })
         );
+  });
 
+      it("visiting Manual data upload screen",()=>{
 
         const url = Cypress.config().baseUrl
         cy.visit(url + '/#/dashboard/manual_data_upload')
 		    cy.log('Load Landing Page')
 		    cy.url().should('eq', url + '/#/dashboard/manual_data_upload')
-        cy.get('.ant-btn > a').click();
-        cy.get('.ant-btn-primary > :nth-child(1)').click();
         
-        cy.get('.steps-action')
+        
+      });
 
-        cy.log("Un Authorized user")
-        cy.get('input[type=file]').selectFile({contents:'cypress_manual_duplicate_no_approved_and_approved_data.xlsx'},{ force: true })
-        cy.wait(3000)
-        cy.get('.ant-alert-close-icon').click()
+        it("download template",()=>{
+              cy.get('.ant-btn > a').click();
+              cy.get('.ant-btn-primary > :nth-child(1)').click();
+        })
+
+        it("Un Authorized user",()=>{
+          cy.wait(3000)
+          cy.log("Un Authorized user")
+          cy.get('input[type=file]').selectFile({contents:'cypress_manual_duplicate_no_approved_and_approved_data.xlsx'},{ force: true })
+          cy.wait(3000)
+          cy.get('.ant-alert-close-icon').click()
+
+        });
+        
 
        
 
-      
+      it("other",()=>{
         cy.log("duplicate approved records")
         cy.get('input[type=file]').selectFile({contents:'cypres_manual_duplicate_approved_data.xlsx'},{ force: true })
         cy.wait(3000)
