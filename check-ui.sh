1d023c143708b1403d389fbb486015effb5286aa
Ubuntu <ubuntu@ip-10-2-131-15.ec2.internal>
2022-06-12 14:04:46 +0000

    cypress docker

--- check-ui.sh
+++ check-ui.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-until curl -s -f -o /dev/null "http://localhost"
+until curl -s -f -o /dev/null "http://nginx"
 do
   sleep 2
   echo " ui container not started yet"
 done
 echo "ui container is started"
-
+DEBUG=code-coverage npm run cy:run
+npx nyc report --reporter=text-summary
+npx nyc report --reporter=text
+npx nyc report --reporter=lcov
