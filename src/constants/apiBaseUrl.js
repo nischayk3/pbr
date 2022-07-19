2feb55052e05e9f2f19a64c636fcd7592ebbe365
Md Fahad Iqbal. Siddiqui <RLE0392@rlpl.com>
2022-07-19 10:43:49 +0530

    url

--- src/constants/apiBaseUrl.js
+++ src/constants/apiBaseUrl.js
@@ -51,7 +51,7 @@ export const MDH_APP_PYTHON_SERVICE = `${process.env.REACT_APP_URL}`;
 
 export const MDH_APP_GENEALOGY = `${process.env.REACT_APP_GENEALOGY}`
 
-export const MDH_AIRFLOW = `${process.env.REACT_APP_URL} + "/airflow/login/?next=https%3A%2F%2F" + ${process.env.REACT_APP_URL} + "%2Fairflow%2Fhome"`
+export const MDH_AIRFLOW = `${process.env.REACT_APP_URL} + "/airflow/login/?next=+ ${process.env.REACT_APP_URL} + "&Fairflow&home"`
 
 export const GOOGLE_ANALYTICS_ID = `${process.env.REACT_APP_GOOGLE_ANALYTICS_ID}`;
 
