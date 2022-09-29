3048492a48702e014d003c6553ccd4dabac4bca1
Dinesh Kumar <RLE0836@rlpl.com>
2022-09-29 14:34:45 +0530

    genealogy fixed

--- cypress/integration/AGenealogy.js
+++ cypress/integration/AGenealogy.js
@@ -28,17 +28,14 @@ describe("Genealogy", () => {
 		cy.visit(url + '/#/dashboard/genealogy')
 		cy.log('Load Landing Page')
 		cy.url().should('eq', url + '/#/dashboard/genealogy')
-		
-		
-
 	});
 
-	it("reload",()=>{
+	it("reload", () => {
 		cy.reload()
 		cy.wait(3000)
 	})
 
-		it("Select plant", () => {
+	it("Select plant", () => {
 		cy.get(
 			":nth-child(1) > .search-block > .ant-select > .ant-select-selector > .ant-select-selection-item"
 		).click();
@@ -81,37 +78,27 @@ describe("Genealogy", () => {
 
 	});
 
-it("click on search button",()=>{
-	cy.get('#genealogy-search').click({ force: true })
-	cy.wait(6000)
-})
-// it("searching node",()=>{
-	
-// 		cy.get('#102279687 > #process-img').click({ force: true })
-// 		cy.get('#view-details-popup > span').click({ force: true })
-// 		cy.wait(6000)
-// 		cy.get('.expand-drawer > img').click({ force: true })
-// 		cy.wait(3000)
-// 		cy.get('.popout-table > .ant-collapse > :nth-child(1) > .ant-collapse-header > .panel-header').click()
-// })
-	
+	it("click on search button", () => {
+		cy.get('#genealogy-search').click({ force: true })
+		cy.wait(6000)
+	})
 
-	it("search again",()=>{
+	it("search again", () => {
 		cy.wait(10000)
-		
+
 	});
 
-	it("search node",()=>{
+	it("search node", () => {
 		cy.get('.ant-tabs-nav-list > :nth-child(2)').click()
 		cy.wait(3000)
 		cy.get('#rc_select_4').click();
-        cy.get('[style="height: 888px; position: relative; overflow: hidden;"] > .rc-virtual-list-holder-inner > .ant-select-item-option-active > .ant-select-item-option-content').click();
-        cy.get('.search-tree > .anticon > svg').click();
-        cy.get('.close-searchicon > .anticon > svg').click();
+		cy.get('[style="height: 888px; position: relative; overflow: hidden;"] > .rc-virtual-list-holder-inner > .ant-select-item-option-active > .ant-select-item-option-content').click();
+		cy.get('.search-tree > .anticon > svg').click();
+		cy.get('.close-searchicon > .anticon > svg').click();
 	});
 
 
-	it("reload",()=>{
+	it("reload", () => {
 		cy.reload()
 		cy.wait(6000)
 	})
@@ -167,42 +154,42 @@ it("click on search button",()=>{
 		cy.wait(3000)
 		cy.get("#view-details-popup").click();
 	});
-	it("closing drawer",()=>{
+	it("closing drawer", () => {
 		cy.wait(5000)
 		cy.get('.ant-drawer-mask').click()
 	})
 
-	it("click on process order",()=>{
+	it("click on process order", () => {
 		cy.wait(5000)
-		cy.get('#node-102279687 > #process-img').click({force:true})
-		cy.get("#view-details-popup").click({force:true});
+		cy.get('#node-102279687 > #process-img').click({ force: true })
+		cy.get("#view-details-popup").click({ force: true });
 	});
-	it("closing drawer",()=>{
+	it("closing drawer", () => {
 		cy.wait(5000)
-		cy.get('.ant-drawer-mask').click({force:true})
-	});
-
-	it("click on material node image",()=>{
-		cy.get('#node-1091460 > #material-img').click({force:true});
-	})
-
-	it("click on backward from node popup",()=>{
-		cy.get('#backword-genealogy-popup > span').click({force:true})
-	})
-
-	it("click on process image",()=>{
-		cy.wait(5000)
-		cy.get("#process-img").click({force:true});
-	})
-
-	it("click on purchase order",()=>{
-		
-		cy.get("#view-details-popup").click({force:true});
+		cy.get('.ant-drawer-mask').click({ force: true })
 	});
 
-	it("closing drawer",()=>{
+	it("click on material node image", () => {
+		cy.get('#node-1091460 > #material-img').click({ force: true });
+	})
+
+	it("click on backward from node popup", () => {
+		cy.get('#backword-genealogy-popup > span').click({ force: true })
+	})
+
+	it("click on process image", () => {
 		cy.wait(5000)
-		cy.get('.ant-drawer-mask').click({force:true})
+		cy.get("#process-img").click({ force: true });
+	})
+
+	it("click on purchase order", () => {
+
+		cy.get("#view-details-popup").click({ force: true });
+	});
+
+	it("closing drawer", () => {
+		cy.wait(5000)
+		cy.get('.ant-drawer-mask').click({ force: true })
 	});
 
 
@@ -242,66 +229,66 @@ it("click on search button",()=>{
 	// 	cy.wait(3000)
 	// 	cy.get('.ant-modal-close-x').click()
 	// })
-	it("going back to filter screen",()=>{
-		cy.get('#rc-tabs-0-tab-1').click({force:true})
+	it("going back to filter screen", () => {
+		cy.get('#rc-tabs-0-tab-1').click({ force: true })
 	})
 
-	it("click on forward toggle button in filter",()=>{
-		cy.get(':nth-child(2) > .toggle-text').click({force:true})
+	it("click on forward toggle button in filter", () => {
+		cy.get(':nth-child(2) > .toggle-text').click({ force: true })
 		cy.wait(3000)
-		cy.get('#genealogy-search').click({force:true})
+		cy.get('#genealogy-search').click({ force: true })
 	})
 
-	it("remove product type and search",()=>{
-		cy.get(':nth-child(4) > .search-block > .ant-btn > .anticon > svg').click({force:true})
-		cy.get('#genealogy-search').click({force:true})
+	it("remove product type and search", () => {
+		cy.get(':nth-child(4) > .search-block > .ant-btn > .anticon > svg').click({ force: true })
+		cy.get('#genealogy-search').click({ force: true })
 	})
 
-	it("back to filter ",()=>{
-		cy.get('#rc-tabs-0-tab-1').click({force:true})
+	it("back to filter ", () => {
+		cy.get('#rc-tabs-0-tab-1').click({ force: true })
 	})
 
 
 	// Gives bad request
-	it("search forward from filter",()=>{
-	  cy.get("#rc-tabs-0-tab-1").click({force:true})
-	  cy.get(".toggleLabel").eq(1).click({force:true})
-	  cy.wait(3000)
-	  cy.get("#genealogy-search").click({force:true})
-	  cy.wait(3000)
-
-	})
-
-	it("Clear filters",()=>{
-	  cy.get('#rc-tabs-0-tab-1').click({force:true})
-	  cy.get("#clear-search").click({force:true})
-	})
-
-	it("search item",()=>{
+	it("search forward from filter", () => {
+		cy.get("#rc-tabs-0-tab-1").click({ force: true })
+		cy.get(".toggleLabel").eq(1).click({ force: true })
 		cy.wait(3000)
-		cy.get("#clear-search").click({force:true})
+		cy.get("#genealogy-search").click({ force: true })
+		cy.wait(3000)
+
 	})
 
-	it("reload for backward 404",()=>{
+	it("Clear filters", () => {
+		cy.get('#rc-tabs-0-tab-1').click({ force: true })
+		cy.get("#clear-search").click({ force: true })
+	})
+
+	it("search item", () => {
+		cy.wait(3000)
+		cy.get("#clear-search").click({ force: true })
+	})
+
+	it("reload for backward 404", () => {
 		cy.reload()
 		cy.wait(3000)
 	})
 
-	
-	it("genealogy 400 error on backward search",()=>{
+
+	it("genealogy 400 error on backward search", () => {
 		cy.intercept(
-			          {
-			            method: 'GET', 
-			            url: '/mdhgenealogy/v1/genealogy?levels=5&batch_id=1338%7C1089084%7C394154&backward=true&mat_type=%27RAW%27',
-					   
-			          },
-							{
-								"detail": "Genealogy with batchId: 1338|1089084|394154 and levels 5 not found",
-								"status": 400,
-								"title": "Bad Request",
-								"type": "about:blank"
-							  }
-			        ).as('genealogy-serach')
+			{
+				method: 'GET',
+				url: '/mdhgenealogy/v1/genealogy?levels=5&batch_id=1338%7C1089084%7C394154&backward=true&mat_type=%27RAW%27',
+
+			},
+			{
+				"detail": "Genealogy with batchId: 1338|1089084|394154 and levels 5 not found",
+				"status": 400,
+				"title": "Bad Request",
+				"type": "about:blank"
+			}
+		).as('genealogy-serach')
 	})
 
 	it("Select plant", () => {
@@ -347,20 +334,20 @@ it("click on search button",()=>{
 
 	});
 
-it("click on search button",()=>{
-	cy.get('#genealogy-search').click({ force: true })
-	cy.wait(6000)
-})
+	it("click on search button", () => {
+		cy.get('#genealogy-search').click({ force: true })
+		cy.wait(6000)
+	})
 
-it("Click on node",()=>{
-	cy.reload()
-	cy.wait(3000)
-})
+	it("Click on node", () => {
+		cy.reload()
+		cy.wait(3000)
+	})
 
-it("Reload final",()=>{
-	cy.reload()
-	cy.wait(3000)
-})
+	it("Reload final", () => {
+		cy.reload()
+		cy.wait(3000)
+	})
 
 
 });
