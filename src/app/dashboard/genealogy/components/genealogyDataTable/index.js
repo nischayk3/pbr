39db0d1bc833710a94cdd5bfc4b3f5fb38548d62
Dinesh Kumar <RLE0836@rlpl.com>
2022-11-02 21:43:16 +0530

    procees capbility UI & genealogy table filter

--- src/app/dashboard/genealogy/components/genealogyDataTable/index.js
+++ src/app/dashboard/genealogy/components/genealogyDataTable/index.js
@@ -5,9 +5,10 @@
  * @Last Modified - 15 March, 2022
  * @Last Changed By - Dinesh Kumar
  */
-import { Button, Collapse, Table } from "antd";
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Collapse, Input, Space, Table } from "antd";
 import React, { useEffect, useState } from "react";
-
+import Highlighter from "react-highlight-words";
 /* istanbul ignore next */
 function GenealogyDataTable(props) {
 	const [batchData, setbatchData] = useState({});
@@ -17,49 +18,157 @@ function GenealogyDataTable(props) {
 	const [proOutput, setProOutput] = useState([]);
 	const [purchaseData, setPurchaseData] = useState([]);
 	const [batchEqupData, setBatchEqupData] = useState([]);
-
+	const [searchedColumn, setSearchedColumn] = useState('');
+	const [searchText, setSearchText] = useState("");
 
 	const { Panel } = Collapse;
 
+	/* istanbul ignore next */
+	function getColumnSearchProps(dataIndex) {
+		return {
+			filterDropdown: ({
+				setSelectedKeys,
+				selectedKeys,
+				confirm,
+				clearFilters,
+			}) => (
+				<div style={{ padding: 8 }}>
+					<Input
+						placeholder={`Search ${dataIndex}`}
+						value={selectedKeys[0]}
+						onChange={(e) =>
+							setSelectedKeys(
+								e.target.value ? [e.target.value] : []
+							)
+						}
+						onPressEnter={() =>
+							handleSearch(selectedKeys, confirm, dataIndex)
+						}
+						style={{
+							marginBottom: 8,
+							display: 'block',
+						}}
+					/>
+					<Space>
+						<Button
+							type='primary'
+							onClick={() =>
+								handleSearch(selectedKeys, confirm, dataIndex)
+							}
+							icon={<SearchOutlined />}
+							size='small'
+							style={{ width: 90 }}
+						>
+							Search
+						</Button>
+						<Button
+							onClick={() => clearFilters && handleReset(clearFilters)}
+							size='small'
+							style={{ width: 90 }}
+						>
+							Reset
+						</Button>
+						<Button
+							type='link'
+							size='small'
+							onClick={() => {
+								confirm({ closeDropdown: false });
+								setSearchText(selectedKeys[0]);
+								setSearchedColumn(dataIndex);
+							}}
+						>
+							Filter
+						</Button>
+					</Space>
+				</div>
+			),
+			filterIcon: (filtered) => (
+				<SearchOutlined
+					style={{ color: filtered ? '#1890ff' : undefined }}
+				/>
+			),
+			onFilter: (value, record) =>
+				record[dataIndex]
+					? record[dataIndex]
+						.toString()
+						.toLowerCase()
+						.includes(value.toLowerCase())
+					: "",
+			onFilterDropdownVisibleChange: (visible) => {
+				if (visible) {
+					// setTimeout(() => this.searchInput.select());
+				}
+			},
+			render: (text) =>
+				searchedColumn === dataIndex ? (
+					<Highlighter
+						highlightStyle={{
+							backgroundColor: '#ffc069',
+							padding: 0,
+						}}
+						searchWords={[searchText]}
+						autoEscape
+						textToHighlight={text.toString()}
+					/>
+				) : (
+					text
+				),
+		};
+	};
+
+	/* istanbul ignore next */
+	function handleSearch(selectedKeys, confirm, dataIndex) {
+		confirm();
+		setSearchText(selectedKeys[0]);
+		setSearchedColumn(dataIndex);
+	}
+
+	/* istanbul ignore next */
+	function handleReset(clearFilters) {
+		clearFilters();
+		setSearchText('');
+	}
+
 	const pbrColumns = [
-		{ title: "Site", dataIndex: "site_code", key: "1", width: 100 },
-		{ title: "Product", dataIndex: "product_num", key: "2", width: 100 },
-		{ title: "Batch", dataIndex: "batch_num", key: "3", width: 100 },
-		{ title: "Parameter", dataIndex: "parameter_name", key: "4", width: 100 },
-		{ title: "Param Value", dataIndex: "parameter_value", key: "5", width: 100 },
-		{ title: "Data Source", dataIndex: "data_source", key: "6", width: 100 },
-		{ title: "System Code", dataIndex: "system_code", key: "7", width: 100 },
-		{ title: "UOM ", dataIndex: "uom_code", key: "8", width: 100 },
+		{ title: "Site", dataIndex: "site_code", key: "1", width: 100, ...getColumnSearchProps('site_code') },
+		{ title: "Product", dataIndex: "product_num", key: "2", width: 100, ...getColumnSearchProps('product_num') },
+		{ title: "Batch", dataIndex: "batch_num", key: "3", width: 100, ...getColumnSearchProps('batch_num') },
+		{ title: "Parameter", dataIndex: "parameter_name", key: "4", width: 100, ...getColumnSearchProps('parameter_name') },
+		{ title: "Param Value", dataIndex: "parameter_value", key: "5", width: 100, ...getColumnSearchProps('parameter_value') },
+		{ title: "Data Source", dataIndex: "data_source", key: "6", width: 100, ...getColumnSearchProps('data_source') },
+		{ title: "System Code", dataIndex: "system_code", key: "7", width: 100, ...getColumnSearchProps('system_code') },
+		{ title: "UOM ", dataIndex: "uom_code", key: "8", width: 100, ...getColumnSearchProps('uom_code') },
 	]
 	const limsColumns = [
-		{ title: "Batch", dataIndex: "batch_num", key: "1", width: 100 },
-		{ title: "Plant", dataIndex: "site_code", key: "2", width: 100 },
-		{ title: "Product", dataIndex: "product_num", key: "3", width: 100 },
+		{ title: "Batch", dataIndex: "batch_num", key: "1", width: 100, ...getColumnSearchProps('batch_num') },
+		{ title: "Plant", dataIndex: "site_code", key: "2", width: 100, ...getColumnSearchProps('site_code') },
+		{ title: "Product", dataIndex: "product_num", key: "3", width: 100, ...getColumnSearchProps('product_num') },
 		{
 			title: "Parameter Name",
 			dataIndex: "parameter_name",
 			key: "4",
-			width: 100
+			width: 100, ...getColumnSearchProps('parameter_name')
 		},
 		{
 			title: "Parameter Value",
 			dataIndex: "parameter_value",
 			key: "5",
-			width: 100
+			width: 100, ...getColumnSearchProps('parameter_value')
 		},
-		{ title: "Data Source", dataIndex: "data_source", key: "6", width: 100 },
-		{ title: "System Code", dataIndex: "system_code", key: "7", width: 100 }
+		{ title: "Data Source", dataIndex: "data_source", key: "6", width: 100, ...getColumnSearchProps('data_source') },
+		{ title: "System Code", dataIndex: "system_code", key: "7", width: 100, ...getColumnSearchProps('system_code') }
 	];
+
 	const processColumns = [
-		{ title: "Process Order", dataIndex: "po_no", key: "1", width: 80 },
-		{ title: "Plant", dataIndex: "plant", key: "2", width: 60 },
-		{ title: "Product", dataIndex: "mat_no", key: "3", width: 60 },
-		{ title: "Batch", dataIndex: "batch_no", key: "4", width: 60 },
-		{ title: "Product Desc", dataIndex: "mat_desc", key: "5", width: 100 },
-		{ title: "Product Type", dataIndex: "mat_type", key: "6", width: 80 },
-		{ title: "Node Id", dataIndex: "node_id", key: "7", width: 100 },
-		{ title: "Unit", dataIndex: "unit", key: "8", width: 70 },
-		{ title: "Qty", dataIndex: "qty", key: "9", width: 70 }
+		{ title: "Process Order", dataIndex: "po_no", key: "1", width: 80, ...getColumnSearchProps('po_no') },
+		{ title: "Plant", dataIndex: "plant", key: "2", width: 60, ...getColumnSearchProps('plant') },
+		{ title: "Product", dataIndex: "mat_no", key: "3", width: 60, ...getColumnSearchProps('mat_no') },
+		{ title: "Batch", dataIndex: "batch_no", key: "4", width: 60, ...getColumnSearchProps('batch_no') },
+		{ title: "Product Desc", dataIndex: "mat_desc", key: "5", width: 100, ...getColumnSearchProps('mat_desc') },
+		{ title: "Product Type", dataIndex: "mat_type", key: "6", width: 80, ...getColumnSearchProps('mat_type') },
+		{ title: "Node Id", dataIndex: "node_id", key: "7", width: 100, ...getColumnSearchProps('node_id') },
+		{ title: "Unit", dataIndex: "unit", key: "8", width: 70, ...getColumnSearchProps('unit') },
+		{ title: "Qty", dataIndex: "qty", key: "9", width: 70, ...getColumnSearchProps('qty') }
 	];
 
 	const toTimestamp = (strDate) => {
@@ -68,8 +177,8 @@ function GenealogyDataTable(props) {
 	}
 
 	const batchEquColumn = [
-		{ title: "Batch", dataIndex: "batch_no", key: "1", width: 80 },
-		{ title: "Process Order", dataIndex: "process_order", key: "2", width: 80 },
+		{ title: "Batch", dataIndex: "batch_no", key: "1", width: 80, ...getColumnSearchProps('batch_no') },
+		{ title: "Process Order", dataIndex: "process_order", key: "2", width: 80, ...getColumnSearchProps('process_order') },
 		{
 			title: "Equipment Id",
 			dataIndex: "equipment_id",
@@ -86,34 +195,14 @@ function GenealogyDataTable(props) {
 						{text}
 					</a>
 				);
-			}
+			},
+			...getColumnSearchProps('equipment_id')
 		},
-		{ title: "Start Time", dataIndex: "start_date", key: "4", width: 80 },
-		{ title: "End Time", dataIndex: "end_date", key: "5", width: 80 }
+		{ title: "Start Time", dataIndex: "start_date", key: "4", width: 80, ...getColumnSearchProps('start_date') },
+		{ title: "End Time", dataIndex: "end_date", key: "5", width: 80, ...getColumnSearchProps('end_date') }
 	];
 
-
-
-
-	//	https://mdh-dashboard.mareana.com/d/trqNakk7z/live-sensor-monitor?orgId=1&from=1515646800000&to=1516770000000
-
-	// const batchEquData = [
-	// 	{
-	// 		po_no: "102250832",
-	// 		referenced: "CELL_CULTURECC_PRODSTR271",
-	// 		start_time: "3/3/20 23:35",
-	// 		end_time: "3/19/20 12:11"
-	// 	},
-	// 	{
-	// 		po_no: "102250832",
-	// 		referenced: "CENTRIFUGECENTRIFUGE_PCCE40110",
-	// 		start_time: "3/19/20 12:05",
-	// 		end_time: "3/19/20 12:06"
-	// 	}
-	// ];
-
 	useEffect(() => {
-		console.log("batchEquData", props.batchEquData);
 		if (props && props.batchInfo) {
 			setbatchData(props.batchInfo);
 		}
