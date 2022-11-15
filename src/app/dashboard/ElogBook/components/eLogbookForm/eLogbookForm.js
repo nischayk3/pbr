1da8e1359dd3467af8f00be0a6de2114f751916c
Siddesh Jaydevappa <RLE1197@rlpl.com>
2022-11-15 11:05:24 +0530

    E log book new template page

---
+++ src/app/dashboard/ElogBook/components/eLogbookForm/eLogbookForm.js
@@ -0,0 +1,91 @@
+/**
+ * @author Siddesh
+ * @Last Modified - 15-11-2022
+ * @Last Changed By - Siddesh
+ */
+import React, { useState } from 'react'
+import {
+	Button,
+	Modal,
+	Row,
+	Col
+} from 'antd';
+import Banner from '../../../../../assets/images/Popup-Side.svg';
+import "./eLogbookForm.scss";
+import InputField from '../../../../../components/InputField/InputField';
+import SelectField from '../../../../../components/SelectField/SelectField';
+
+function ElogForm(props) {
+
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const [hierarchyName, setHierarchyName] = useState('')
+	const handleCancel = () => {
+		setIsModalVisible(false);
+	};
+
+	return (
+		
+			<Modal
+          className="landing-modal"
+          title="Basic details - New template [Draft]"
+          visible={true}
+          onCancel={handleCancel}
+          centered
+          footer={[
+            <Row>
+                  <Col span={4} offset={16} >
+                    <Button
+                      className="custom-primary-btn "
+                      type="primary"
+                    // onClick={() =>  handleBack()}
+                    >
+                      Back
+                    </Button>
+                  </Col>
+                  <Col span={4} style={{ textAlign: "end" }}>
+                    <Button
+                      className="custom-secondary-btn"
+                      type="primary"
+                    // onClick={() =>  handleNext()}
+                    >
+                      Lets Go!
+                    </Button>
+                  </Col>
+                </Row>
+          ]}
+        >
+          <Row>
+            <Col span={12}>             <img src={Banner} style={{ paddingTop: "22px" }} alt="Ebookpng" />
+           </Col>
+            <Col span={12}>
+             <div>
+                 <InputField
+                  label="Template name"
+                  type="text"
+                  name="Tname"
+                  placeholder="Enter name of new template"
+                  //  value={props.value}
+                  // onChange={handleChange}
+                />
+              </div>
+              <br />
+              <div>
+                <SelectField
+                  label="What product is this template for?"
+                  placeholder="Select product"
+                  name="Pname"
+                  // value={props.selectedValue}
+                  // onChange={handleChange}
+                  style={{ width: "100%", margin: "0px" }}
+                />
+              </div>
+            </Col>
+
+          </Row>
+        </Modal>
+
+	)
+}
+
+
+export default ElogForm
--- src/app/dashboard/ElogBook/components/elogBook.js
+++ src/app/dashboard/ElogBook/components/elogBook.js
@@ -4,16 +4,19 @@
  * @version  1
  * @Last Modified - 08 Nov, 2022
  * @Last Changed By - Dinesh
+ * @Last Modified - 15-11-2022
+ * @Last Changed By - Siddesh
  */
 
 import React from 'react';
 import BreadCrumbWrapper from "../../../../components/BreadCrumbWrapper";
+import Elogbook from './eLogbookForm/eLogbookForm';
 const ElogBook = () => {
 	return (
 		<div className="custom-wrapper">
 			<BreadCrumbWrapper />
 			<div className="custom-content-layout">
-				<p>elog book</p>
+				<Elogbook />
 			</div>
 		</div>
 	)
