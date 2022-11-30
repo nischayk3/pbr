c57f56842e86ebcface26566a585ea98201d7ef5
Vinay <vinay.palagati1.com>
2022-11-30 11:14:46 +0530

    fixed-target-variable

--- src/app/dashboard/Analysis/components/ViewPage/model/Model.js
+++ src/app/dashboard/Analysis/components/ViewPage/model/Model.js
@@ -177,7 +177,6 @@ const Model = ({ finalModelJson, setFinalModelJson, editFinalJson, tableKey, mod
       let targetVariable = apiResponse.data?.target_variable;
       let getJson;
       if (finalJson?.variable_mapping?.length >= 1) {
-        finalJson.target_variable = targetVariable
         setFinalModelJson(finalJson)
       } else {
         getJson = await getModelJson(apiResponse.data);
