45dc5c0180e79ccce5cb5e0dc1d9b296a36e67ae
Dinesh Kumar <RLE0836@rlpl.com>
2023-01-16 18:15:25 +0530

    user roles & tableu dashboard ui

---
+++ src/app/dashboard/UserRolesAndAccess/RolesAndAccess/Roles.js
@@ -0,0 +1,221 @@
+/**
+ * @author Dinesh Kumar <dinesh.kumar@mareana.com>
+ * @Mareana - Release 3.5
+ * @version  1
+ * @Last Modified - 13 Jan, 2023
+ * @Last Changed By - Dinesh Kumar
+ */
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { Badge, Button, Input, List, Space, Switch, Table, Tag } from 'antd';
+import React, { useState } from 'react';
+
+const Roles = () => {
+	const [isCreateRole, setIsCreateRole] = useState(false);
+
+	const data = [
+		'Chart manager',
+		'Default',
+		'Old approver',
+		'View creator',
+		'View',
+	];
+
+	const listHeader = (
+		<div className="roles-head">
+			<p>Role</p>
+			{isCreateRole && (
+				<div className="create-role">
+					<Input placeholder="Enter name of new role" />
+					<Button type="link" >
+						Save
+					</Button>
+				</div>
+			)}
+		</div>
+	)
+
+	const resourceCard = (
+		<div className="resource-card">
+			<div className="resource-card-head">
+				<div className='card-head'>
+					<p>3 AUTHORIZATIONS</p>
+					<h2>GENEALOGY</h2>
+				</div>
+				<div className="resource-card-button">
+					<EditOutlined />
+					<DeleteOutlined />
+				</div>
+			</div>
+			<p>Chart managers can access Genealogy with the authorizations given below.</p>
+			<div className="auth-card">
+				<p>AUTHORIZATIONS</p>
+				<Tag className="status-tag">DISPLAY</Tag>
+				<Tag className="status-tag">MAINTAIN</Tag>
+			</div>
+		</div>
+	)
+
+	const handleClickROle = () => {
+		setIsCreateRole(true)
+	}
+
+	const expandedRowRender = () => {
+		const columns1 = [
+			{
+				title: 'Date',
+				dataIndex: 'date',
+				key: 'date',
+			},
+			{
+				title: 'Name',
+				dataIndex: 'name',
+				key: 'name',
+			},
+			{
+				title: 'Status',
+				key: 'state',
+				render: () => <Badge status="success" text="Finished" />,
+			},
+			{
+				title: 'Upgrade Status',
+				dataIndex: 'upgradeNum',
+				key: 'upgradeNum',
+			},
+			{
+				title: 'Action',
+				dataIndex: 'operation',
+				key: 'operation',
+				render: () => (
+					<Space size="middle">
+						<a>Pause</a>
+						<a>Stop</a>
+
+					</Space>
+				),
+			},
+		];
+		const data1 = [];
+		for (let i = 0; i < 3; ++i) {
+			data1.push({
+				key: i.toString(),
+				date: '2014-12-24 23:12:00',
+				name: 'This is production name',
+				upgradeNum: 'Upgraded: 56',
+			});
+		}
+		return <Table columns={columns1} dataSource={data1} pagination={false} />;
+	};
+	const columns2 = [
+		{
+			title: 'Name',
+			dataIndex: 'name',
+			key: 'name',
+		},
+		{
+			title: 'Platform',
+			dataIndex: 'platform',
+			key: 'platform',
+		},
+		{
+			title: 'Version',
+			dataIndex: 'version',
+			key: 'version',
+		},
+		{
+			title: 'Upgraded',
+			dataIndex: 'upgradeNum',
+			key: 'upgradeNum',
+		},
+		{
+			title: 'Creator',
+			dataIndex: 'creator',
+			key: 'creator',
+		},
+		{
+			title: 'Date',
+			dataIndex: 'createdAt',
+			key: 'createdAt',
+		},
+		{
+			title: 'Action',
+			key: 'operation',
+			render: () => <a>Publish</a>,
+		},
+	];
+	const data2 = [];
+	for (let i = 0; i < 3; ++i) {
+		data2.push({
+			key: i.toString(),
+			name: 'Screen',
+			platform: 'iOS',
+			version: '10.3.4.5654',
+			upgradeNum: 500,
+			creator: 'Jack',
+			createdAt: '2014-12-24 23:12:00',
+		});
+	}
+
+	return (
+		<div className="roles-wrapper">
+			<div className="roles">
+				<div className="roles-button">
+					<Button
+						type='primary'
+						className='custom-secondary-btn'
+						onClick={handleClickROle}
+					>
+						Create role
+					</Button>
+				</div>
+				<List
+					header={listHeader}
+					// footer={<div>Footer</div>}
+					dataSource={data}
+					renderItem={(item) => (
+						<List.Item>
+							{item}
+						</List.Item>
+					)}
+				/>
+			</div>
+			<div className="roles-details">
+				<div className="roles-heading">
+					<h1>Chart Manager</h1>
+					<div className="sub-details">
+						<p>3 resources | Active </p>
+						<Switch size="small" defaultChecked />
+					</div>
+				</div>
+				<p className="content">
+					The chart manager works with sample sentences like this one, and can always be replaced by the correct information since this is only a design placeholder.
+				</p>
+				<p className='card-heading'>Resources and authorizations</p>
+				<div className="resource-card-wrapper">
+					{resourceCard}
+					{resourceCard}
+					{resourceCard}
+					{resourceCard}
+				</div>
+
+				<p className='card-heading'>Data access</p>
+				<Table
+					columns={columns2}
+					expandable={{
+						expandedRowRender,
+						defaultExpandedRowKeys: ['0'],
+					}}
+					dataSource={data2}
+				/>
+				{/* <Empty imageStyle={{
+					height: 160,
+				}} image={Empty.PRESENTED_IMAGE_SIMPLE} description={
+					<span>
+						Select a role to view its details here
+					</span>
+				} /> */}
+			</div>
+		</div>
+	)
+}
+
+export default Roles;
--- src/app/dashboard/UserRolesAndAccess/RolesAndAccess/RolesAndAccess.js
+++ src/app/dashboard/UserRolesAndAccess/RolesAndAccess/RolesAndAccess.js
@@ -6,28 +6,33 @@
  * @Last Changed By - Bhanu Thareja
  */
 
-
-import BreadCrumbWrapper from '../../../../components/BreadCrumbWrapper/index'
-import GoBackSubHeader from '../../../../components/GoBackSubHeader/GoBackSubHeader'
-import EditableTable from '../../../../components/EditableTable/EditableTable'
-
-import { getRoleConfiguartions, saveRoleConfiguartions, deleteRoleConfiguartions } from '../../../../services/userRolesAndAccessService'
+import BreadCrumbWrapper from '../../../../components/BreadCrumbWrapper/index';
+//import EditableTable from '../../../../components/EditableTable/EditableTable'
+import Roles from './Roles';
+import "./style.scss";
 
 const RolesAndAccess = () => {
 
 	return (
-		<>
+		// <>
+		// 	<BreadCrumbWrapper />
+		// 	<div className='custom-user-roles-wrapper'>
+		// 		<GoBackSubHeader currentPage="Roles" />
+		//  <EditableTable
+		// 			getTableData={getRoleConfiguartions}
+		// 			saveTableData={saveRoleConfiguartions}
+		// 			deleteTableRow={deleteRoleConfiguartions}
+		// 			screens = {"Roles"}
+		// 		/>
+		// 	</div>
+		// </>
+		<div className='custom-wrapper'>
 			<BreadCrumbWrapper />
-			<div className='custom-user-roles-wrapper'>
-				<GoBackSubHeader currentPage="Roles" />
-				<EditableTable
-					getTableData={getRoleConfiguartions}
-					saveTableData={saveRoleConfiguartions}
-					deleteTableRow={deleteRoleConfiguartions}
-					screens = {"Roles"}
-				/>
+			<div className='custom-content-layout'>
+				<Roles />
 			</div>
-		</>
+		</div>
+
 	)
 }
 
