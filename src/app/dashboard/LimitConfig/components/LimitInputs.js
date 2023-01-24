79891a51c3f9378b13450654bd98993865b97759
vinay <vinay.reddy@mareana.com>
2023-01-24 08:47:21 +0530

    added molecule-limit configurator

--- src/app/dashboard/LimitConfig/components/LimitInputs.js
+++ src/app/dashboard/LimitConfig/components/LimitInputs.js
@@ -1,40 +1,49 @@
-import React, { useState } from 'react'
-import { Col, Row, Table, Input, DatePicker } from 'antd'
+import React, { useEffect, useState } from 'react'
+import './Limitconfig.scss'
+import SelectField from "../../../../components/SelectField/SelectField";
+import { Col, Row, Table, Input, DatePicker, Button } from 'antd';
+import { DeleteOutlined, CalendarOutlined, PlusOutlined } from '@ant-design/icons'
 import moment from 'moment';
 
-const LimitInputs = ({editTable}) => {
+const LimitInputs = ({ editTable, openRow, selectedRowKey }) => {
 
-    const data = [
-        {
-            key: 1,
-            site: "1655",
-            parameter: "Yeild",
-            limitType: "UCL",
-            from: '6',
-            to: '10',
-            vod: "31-12-2028"
-        },
-        {
-            key: 2,
-            site: "1322",
-            parameter: "Acidity",
-            limitType: "UCL",
-            from: '6',
-            to: '10',
-            vod: "20-12-2028"
-        }
-    ];
-    const [limitsData, setLimitsData] = useState(data);
+	let limitList = [
+		'Control Limits',
+		'Specifications Limits',
+		'Warnings'
+	]
+	const data = [
+		{
+			key: 1,
+			site: "1655",
+			parameter: "Yeild",
+			limitType: "Control Limits",
+			from: '6',
+			to: '10',
+			vod: "31-12-2028"
+		},
+		{
+			key: 2,
+			site: "1322",
+			parameter: "Acidity",
+			limitType: "Control Limits",
+			from: '6',
+			to: '10',
+			vod: "20-12-2028"
+		}
+	];
+	const [limitsData, setLimitsData] = useState(data);
 
-    const columns = [
-        {
-            title: 'Site',
-            dataIndex: 'site',
-            key: 'Site',
-            render: (text, record) =>
-               limitsData.map((data, index) => {
+	let columns = [
+		{
+			title: 'Site',
+			dataIndex: 'site',
+			key: 'Site',
+			width: 150,
+			render: (text, record) =>
+				limitsData.map((data, index) => {
 					if (record.key === data.key) {
-						if (!editTable) {
+						if (selectedRowKey !== openRow) {
 							return <p style={{ margin: "0" }}>{data.site}</p>;
 						}
 						return (
@@ -46,15 +55,15 @@ const LimitInputs = ({editTable}) => {
 						);
 					}
 				}),
-        },
-        {
-            title: 'Parameter',
-            dataIndex: 'parameter',
-            key: 'parameter',
-            render: (text, record) =>
-               limitsData.map((data, index) => {
+		},
+		{
+			title: 'Parameter',
+			dataIndex: 'parameter',
+			key: 'parameter',
+			render: (text, record) =>
+				limitsData.map((data, index) => {
 					if (record.key === data.key) {
-						if (!editTable) {
+						if (selectedRowKey !== openRow) {
 							return <p style={{ margin: "0" }}>{data.parameter}</p>;
 						}
 						return (
@@ -66,36 +75,40 @@ const LimitInputs = ({editTable}) => {
 						);
 					}
 				}),
-        },
-        {
-            title: 'Limit Type',
-            dataIndex: 'limitType',
-            key: 'limitType',
-            render: (text, record) =>
-               limitsData.map((data, index) => {
+		},
+		{
+			title: 'Limit Type',
+			dataIndex: 'limitType',
+			key: 'limitType',
+			render: (text, record) =>
+				limitsData.map((data, index) => {
 					if (record.key === data.key) {
-						if (!editTable) {
+						if (selectedRowKey !== openRow) {
 							return <p style={{ margin: "0" }}>{data.limitType}</p>;
 						}
 						return (
-							<Input
-								name="limitType"
-								value={data.limitType}
-								onChange={(e) => handleChange(index, e, "", "limits")}
+							<SelectField
+							    name="limitType"
+								selectList={limitList}
+								selectedValue={data.limitType}
+								onChangeSelect={(e) => handleChange(index, e, "", "limitType")}
 							/>
 						);
 					}
 				}),
-        },
-        {
-            title: 'From',
-            dataIndex: 'from',
-            key: 'from',
-            render: (text, record) =>
-               limitsData.map((data, index) => {
+		},
+		{
+			title: 'From',
+			dataIndex: 'from',
+			key: 'from',
+			width: 100,
+			render: (text, record) =>
+				limitsData.map((data, index) => {
 					if (record.key === data.key) {
-						if (!editTable) {
-							return <p style={{ margin: "0" }}>{data.from}</p>;
+						if (selectedRowKey !== openRow) {
+							return (<>
+								<p style={{ margin: "0" }}>{data.from}</p>
+							</>);
 						}
 						return (
 							<Input
@@ -106,15 +119,16 @@ const LimitInputs = ({editTable}) => {
 						);
 					}
 				}),
-        },
-        {
-            title: 'To',
-            dataIndex: 'to',
-            key: 'to',
-            render: (text, record) =>
-               limitsData.map((data, index) => {
+		},
+		{
+			title: 'To',
+			dataIndex: 'to',
+			key: 'to',
+			width: 100,
+			render: (text, record) =>
+				limitsData.map((data, index) => {
 					if (record.key === data.key) {
-						if (!editTable) {
+						if (selectedRowKey !== openRow) {
 							return <p style={{ margin: "0" }}>{data.to}</p>;
 						}
 						return (
@@ -126,15 +140,15 @@ const LimitInputs = ({editTable}) => {
 						);
 					}
 				}),
-        },
-        {
-            title: 'Validity Date',
-            dataIndex: 'vod',
-            key: 'vod',
-            render: (text, record) =>
+		},
+		{
+			title: 'Validity Date',
+			dataIndex: 'vod',
+			key: 'vod',
+			render: (text, record) =>
 				limitsData.map((data, index) => {
 					if (record.key === data.key) {
-						if (!editTable) {
+						if (selectedRowKey !== openRow) {
 							if (!data.vod) {
 								return "";
 							} else {
@@ -143,7 +157,10 @@ const LimitInputs = ({editTable}) => {
 								const month = d.getMonth();
 								const day = d.getDate();
 								return (
-									<p style={{ margin: "0" }}>{data.vod}</p>
+									<span>
+										<CalendarOutlined style={{ color: '#6C63FF', display: "inline-block", marginRight: '10px' }} />
+										<p style={{ margin: "0", display: "inline-block" }}>{data.vod}</p>
+									</span>
 								);
 							}
 						}
@@ -162,8 +179,10 @@ const LimitInputs = ({editTable}) => {
 						);
 					}
 				}),
-        },
-    ];
+		},
+	];
+
+	const [tableColumns, setTableCOlumns] = useState();
 
 	const handleChange = (index, event, dateString, type) => {
 		const rowsInput = [...limitsData];
@@ -175,24 +194,58 @@ const LimitInputs = ({editTable}) => {
 		} else if (type === "limits") {
 			const { name, value } = event.target;
 			rowsInput[index][name] = value;
+		} else if(type === 'limitType') {
+			rowsInput[index]['limitType'] = event;
 		}
 		setLimitsData(rowsInput);
 	};
 
-    return (
-        <div className='expand-table'>
-            <Row>
-                <Col span={1} className="empty-space"/>
-                <Col span={23}>
-                    <Table
-                        columns={columns}
-                        pagination={false}
-                        dataSource={limitsData}
-                    />
-                </Col>
-            </Row>
-        </div>
-    )
+
+	useEffect(() => {
+		const obj = {
+			title: '',
+			width: 10,
+			key: 'delete',
+			render: () => {
+				return (
+					<DeleteOutlined style={{ fontSize: '18px' }} />
+				)
+			}
+		}
+		if (selectedRowKey === openRow) {
+			columns.unshift(obj)
+		} else {
+			columns = columns.filter((ele) => ele?.key !== 'delete')
+		}
+		setTableCOlumns(columns)
+	}, [selectedRowKey, openRow, limitsData])
+
+	return (
+		<div className='expand-table'>
+			<Row>
+				<Col span={1} className="empty-space" />
+				<Col span={23}>
+					<Table
+						columns={tableColumns}
+						pagination={false}
+						dataSource={limitsData}
+					/>
+					{(selectedRowKey === openRow) && <div className="add-button-limit">
+						<Button
+						// onClick={() => handleAdd()}
+						// disabled={
+						// 	Object.keys(params).length > 0 &&
+						// 	params.fromScreen !== "Workspace"
+						// }
+						>
+							<PlusOutlined />
+							Add new parameter
+						</Button>
+					</div>}
+				</Col>
+			</Row>
+		</div>
+	)
 }
 
 export default LimitInputs
--- src/app/dashboard/LimitConfig/components/LimitTable.js
+++ src/app/dashboard/LimitConfig/components/LimitTable.js
@@ -1,43 +1,33 @@
 import React, { useState } from 'react'
 import './Limitconfig.scss'
-import { Table, Button } from 'antd'
+import { Table, Button, Dropdown, Space, Select } from 'antd'
+import { DownOutlined, UploadOutlined, DownloadOutlined } from '@ant-design/icons';
+import SelectField from "../../../../components/SelectField/SelectField";
 import LimitInputs from './LimitInputs';
 
 const LimitTable = () => {
 
   const [editTable, setEditTable] = useState(false)
-
-  const columns = [
+  const [openRow, setOpenRow] = useState();
+  const items = [
     {
-      title: 'Molecule',
-      dataIndex: 'molecule',
-      key: 'molecule',
-      width: 1000
+      label: <div><DownloadOutlined />  &nbsp; Download template</div>,
+      key: '0',
     },
     {
-      title: 'Action',
-      dataIndex: '',
-      key: 'x',
-      render: () => {
-        return (
-          <div className='action-table'>
-            {!editTable && <a onClick={() => setEditTable(!editTable)}>Edit</a> }
-            {editTable && <a onClick={() => setEditTable(!editTable)}>Save</a> }
-            <a>Delete</a>
-          </div>
-        )
-      },
+      label: <div><UploadOutlined /> &nbsp; Upload filled template</div>,
+      key: '1',
     },
   ];
 
   const data = [
     {
       key: 1,
-      molecule: 'Belactpet',
+      molecule: 'Elixr-1',
     },
     {
       key: 2,
-      molecule: 'Elixr',
+      molecule: 'Elixr-2',
     },
     {
       key: 3,
@@ -45,9 +35,85 @@ const LimitTable = () => {
     },
     {
       key: 4,
-      molecule: 'Belactpet',
+      molecule: 'Elixr-3',
     },
   ];
+
+  const molList = [
+    {
+      value: 'Elixr-1',
+      label: 'Elixr-1',
+    },
+    {
+      value: 'Elixr-2',
+      label: 'Elixr-2',
+    },
+    {
+      value: 'Elixr-3',
+      label: 'Elixr-3',
+    },
+    {
+      value: 'TestinKp',
+      label: 'TestinKp',
+    }
+  ]
+
+  const columns = [
+    {
+      title: 'Molecule',
+      dataIndex: 'molecule',
+      key: 'molecule',
+      width: 1000,
+      render: (text, record) =>
+        data.map((data, index) => {
+          if (record.key === data.key) {
+            if (record.key === openRow) {
+              return (
+                <Select
+                  name="molecule"
+                  style={{width: '70%'}}
+                  options={molList}
+                  value={data.molecule}
+                  onClick={(e) => e.stopPropagation()}
+                  // onChangeSelect={(e) => handleChange(index, e, "", "limitType")}
+                />
+              );
+            } else {
+              return <p style={{ margin: "0" }}>{data.molecule}</p>;
+            }
+          }
+        }),
+    },
+    {
+      title: 'Action',
+      dataIndex: '',
+      key: 'x',
+      render: (text, record) => {
+        return (
+          <div className='action-table'>
+            {(record.key !== openRow) && <a onClick={(e) => onEdit(e, record.key)}>Edit</a>}
+            {(record.key === openRow) && <a onClick={() => setOpenRow('')}>Save</a>}
+            <a onClick={(e) => e.stopPropagation()}>Delete</a>
+            {(record.key === openRow) && <Dropdown menu={{ items }} trigger={['click']}>
+              <a onClick={(e) => e.stopPropagation()}>
+                <Space>
+                  More
+                  <DownOutlined />
+                </Space>
+              </a>
+            </Dropdown>}
+          </div>
+        )
+      },
+    },
+  ];
+
+  const onEdit = (e, rowKey) => {
+    if (editTable) {
+      e.stopPropagation();
+    }
+    setOpenRow(rowKey)
+  }
 
   return (
     <div className='limit-container'>
@@ -57,12 +123,21 @@ const LimitTable = () => {
             columns={columns}
             pagination={false}
             expandable={{
-              expandedRowRender: (record) => <LimitInputs editTable={editTable} />,
-              rowExpandable: (record) => record.name !== 'Not Expandable',
-              expandRowByClick: true
+              expandedRowRender: (record) => <LimitInputs editTable={editTable} selectedRowKey={record.key} openRow={openRow} />,
+              // rowExpandable: (record) => record.name !== 'Not Expandable',
+              expandRowByClick: true,
+              onExpand: (expanded, record) => {
+                if (!expanded) {
+                  setEditTable(false)
+                  setOpenRow('');
+                } else {
+                  setEditTable(true)
+                }
+              }
             }}
             // expandRowByClick={true}
             dataSource={data}
+            rowKey={(record) => record.key}
           />
         </div>
       </div>
