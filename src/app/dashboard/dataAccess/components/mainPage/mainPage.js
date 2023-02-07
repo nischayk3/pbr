6957e8d5f72416098340e1eb03ea46c441ed990c
Mihir Bagga <mihir.bagga@mareana.com>
2023-02-07 19:39:55 +0530

    BR-2912 View added in data access screen

--- src/app/dashboard/dataAccess/components/mainPage/mainPage.js
+++ src/app/dashboard/dataAccess/components/mainPage/mainPage.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import DataAccessLandingPage from "../landingPage/landing";
 import DataAccessTabs from "../tabs/tabsList";
 import DataAccessContentArea from "../contentArea/contentArea";
 import { Col, Row } from "antd";
-import './mainPage.scss'
-
-
+import "./mainPage.scss";
 
 export default function DataAccessMainPage() {
-    return (
-        <div>
-            <DataAccessLandingPage />
-            <div className="rows">
-                <Row >
-                    <Col span={6}> <DataAccessTabs /></Col>
-                    <Col span={18}><DataAccessContentArea /></Col>
-                </Row>
-            </div>
-        </div>
-    )
-
+  const [selectedTab, setSelectedTab] = useState("Genealogy");
+  return (
+    <div>
+      <DataAccessLandingPage />
+      <div className="rows">
+        <Row>
+          <Col span={6}>
+            {" "}
+            <DataAccessTabs setSelectedTab={setSelectedTab} />
+          </Col>
+          <Col span={18}>
+            <DataAccessContentArea selectedTab={selectedTab} />
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 }
