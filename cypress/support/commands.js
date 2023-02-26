7d6e00722b0381683ad11b32c18347d037789b1f
Dinesh Kumar <RLE0836@rlpl.com>
2023-02-26 17:59:19 +0530

    failed testcases fixed

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -258,3 +258,221 @@ Cypress.Commands.add('loginWithAD', () => {
 
 	})
 })
+
+Cypress.Commands.add('loginWithoutAD', () => {
+	cy.window().then((window) => {
+		window.localStorage.setItem("test_enabled", true);
+		localStorage.setItem("loginwith", "WITHOUT_AD");
+		window.localStorage.setItem("user", "dinesh.jinjala@mareana.com");
+		window.localStorage.setItem("username", "Dinesh");
+		window.localStorage.setItem(
+			"login_details", JSON.stringify({
+				ad_role: false,
+				email_id: "dinesh.jinjala@mareana.com",
+				user_id: "dinesh.kumar@mareana.com",
+				firstname: "Dinesh",
+				lastname: "Jinjala",
+				mdh_role: ['SYSTEM_ADMIN'],
+				screen_set: "1000_USER",
+				resource_action: {
+					"AUDIT_REPORT": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 10
+					},
+					"AUTO_ML": {
+						"actions": [
+							"READ",
+							"MAINT",
+							"APPR"
+						],
+						"resource_id": 14
+					},
+					"CHART": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 2
+					},
+					"CROSS_BATCH": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 15
+					},
+					"DASHBOARD": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 4
+					},
+					"DATA_ACCESS": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 23
+					},
+					"DSS": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 12
+					},
+					"ELOG_BOOK_DATA_ENTRY": {
+						"actions": [
+							"APPR",
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 21
+					},
+					"ELOG_BOOK_TEMPLATE_CREATION": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 22
+					},
+					"GENEALOGY": {
+						"actions": [
+							"READ",
+							"UPLOAD",
+							"MAINT"
+						],
+						"resource_id": 3
+					},
+					"HIERARCHY_CONFIG": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 11
+					},
+					"LIMIT_CONFIG": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 25
+					},
+					"PARAM_DATA_FILE_UPLOAD": {
+						"actions": [
+							"READ",
+							"UPLOAD"
+						],
+						"resource_id": 5
+					},
+					"PBR_DASHBOARD": {
+						"actions": [
+							"READ",
+							"MAINT",
+							"APPR"
+						],
+						"resource_id": 17
+					},
+					"PBR_FILE_SEARCH": {
+						"actions": [
+							"READ"
+						],
+						"resource_id": 27
+					},
+					"PBR_FILE_UPLOAD": {
+						"actions": [
+							"MAINT",
+							"READ",
+							"UPLOAD"
+						],
+						"resource_id": 18
+					},
+					"PBR_TEMPLATE": {
+						"actions": [
+							"READ",
+							"MAINT",
+							"APPR"
+						],
+						"resource_id": 16
+					},
+					"REPORT_DESIGNER": {
+						"actions": [
+							"READ",
+							"MAINT",
+							"APPR"
+						],
+						"resource_id": 8
+					},
+					"REPORT_GENERATOR": {
+						"actions": [
+							"READ",
+							"MAINT",
+							"APPR"
+						],
+						"resource_id": 6
+					},
+					"ROLES_CONFIG": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 20
+					},
+					"SYSTEM_CONFIG": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 26
+					},
+					"TABLEAU_DASHBOARD": {
+						"actions": [
+							"READ",
+							"MAINT"
+						],
+						"resource_id": 24
+					},
+					"USER_CONFIG": {
+						"actions": [
+							"READ",
+							"DOWNLOAD",
+							"APPR",
+							"UPLOAD",
+							"MAINT"
+						],
+						"resource_id": 19
+					},
+					"USER_REPORT": {
+						"actions": [
+							"READ"
+						],
+						"resource_id": 13
+					},
+					"VIEW": {
+						"actions": [
+							"READ",
+							"MAINT",
+							"APPR"
+						],
+						"resource_id": 1
+					},
+					"WORKITEMS": {
+						"actions": [
+							"READ",
+							"MAINT",
+							"APPR"
+						],
+						"resource_id": 7
+					}
+				},
+				token:
+					"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IkRpbmVzaCAgSmluamFsYSIsInVuaXhfdGltZXN0YW1wIjoxNjc3MDU2NzE1LjY0MTg5MywidGltZXN0YW1wIjoiMjIvMDIvMjAyMyAwOTowNToxNSIsImV4cCI6MzMyMTMwNTY3MTUsImFkX3JvbGUiOmZhbHNlLCJtZGhfcm9sZSI6IlNZU1RFTV9BRE1JTiIsImVtYWlsX2lkIjoiZGluZXNoLmppbmphbGFAbWFyZWFuYS5jb20iLCJjdXN0X2tleSI6IjEwMDAifQ.j5ZwCYeiIBFHregUuKeA_MLfGZ7U0_qBCBkwsUWnKOk"
+			})
+		);
+
+	})
+})
