b8170e51136399a51d14ae1567903c7b2cfe59bc
Dinesh Kumar <RLE0836@rlpl.com>
2023-03-30 19:14:33 +0530

    proces hiearchy folder structure ui created

--- src/components/ContextMenu/ContextMenu.js
+++ src/components/ContextMenu/ContextMenu.js
@@ -3,11 +3,11 @@ import React from 'react';
 import './ContextMenu.scss';
 
 const ContextMenu = (props) => {
-	const { x, y, show, item, } = props;
-
+	const { x, y, show, item, handleClick } = props;
+	const contextMenu = document.getElementById("popup")
 	window.onclick = function (event) {
-		if (event.target.className.baseVal !== "") {
-			document.getElementById("#popup").style("display", "none");
+		if (event.target.contextMenu !== contextMenu) {
+			props.onClose(false)
 		}
 	};
 
@@ -16,7 +16,8 @@ const ContextMenu = (props) => {
 			id="popup"
 			className='context__menu'
 			style={{
-				position: 'absolute',
+				position: 'fixed',
+				zIndex: 10000,
 				left: `${x}px`,
 				top: `${y}px`,
 				display: show ? 'block' : 'none',
@@ -24,8 +25,8 @@ const ContextMenu = (props) => {
 		>
 			<ul>
 				{item.map((e, i) => {
-					return <li key={i}>
-						<Button key={i} className='custom-secondary-btn-link' type='link' icon={e.icon} onClick={props.handleClick(e.eventName)}>
+					return <li key={e.eventName}>
+						<Button disabled={e.visible} key={e.eventName} className='custom-secondary-btn-link' type='link' icon={e.icon} onClick={() => handleClick(e.eventName)}>
 							{e.value}
 						</Button>
 					</li>
